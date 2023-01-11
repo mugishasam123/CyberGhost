189888f7cb3d32efff4fc05854e360e6dab51d16
mugishasam123 <mugishasamuel42@gmail.com>
2023-01-11 22:24:24 +0200

    Add server and protocols

--- src/main/java/com/samuel/client/SimpleClient.java
+++ src/main/java/com/samuel/client/SimpleClient.java
@@ -1,7 +1,45 @@
 package main.java.com.samuel.client;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
 public class SimpleClient {
-    public static void main(String[] args) {
-        
+
+    private static Socket  kkSocket;
+    public static void main(String[] args){
+        try {
+             kkSocket = new Socket("127.0.0.1",8443);
+             
+            PrintWriter out = new PrintWriter(kkSocket.getOutputStream(), true);
+           
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(kkSocket.getInputStream()));
+                   
+            BufferedReader stdIn =
+                    new BufferedReader(new InputStreamReader(System.in));
+                   
+            String fromServer;
+            String fromUser;
+          
+    
+
+            while ((fromServer = in.readLine()) != null) {
+                System.out.println("Server: " + fromServer);
+                if (fromServer.equals("Bye."))
+                    break;
+                System.out.println("Request to the server:");
+                fromUser = stdIn.readLine();
+                if (fromUser != null) {
+                    System.out.println("Client: " + fromUser);
+                    out.println(fromUser);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/com/samuel/server/SimpleServer.java
+++ src/main/java/com/samuel/server/SimpleServer.java
@@ -1,8 +1,43 @@
 package main.java.com.samuel.server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import main.java.com.samuel.protocol.KnockKnockProtocol;
+
 public class SimpleServer {
-    
+
+
+    private static ServerSocket serverSocket ;
     public static void main(String[] args) {
-        
+
+      
+
+        try {
+             serverSocket = new ServerSocket(8443);
+            Socket socketClient = serverSocket.accept();
+
+            PrintWriter out = new PrintWriter(socketClient.getOutputStream(),true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+            String inputLine;
+            String outputLine;
+           
+            KnockKnockProtocol kkp = new KnockKnockProtocol();
+            
+            outputLine = kkp.processInput(null);
+            out.println(outputLine);
+            System.out.println("server started"+outputLine);
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = kkp.processInput(inputLine);
+                out.println(outputLine);
+                if (inputLine.equalsIgnoreCase("bye"))
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
